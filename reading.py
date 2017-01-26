96dea34ede755f1b5a6bcd17500a419edd851c34
Filip-PC <kinez.didi@gmail.com>
2017-01-26 18:53:53 +0100

    dodadena funkcija za citanje na three_band slikite

--- reading.py
+++ reading.py
@@ -9,8 +9,8 @@ def read_3band(img_id=None, train=True):
     :param img_id: ako sakas da procitas edna slika vnesi img_id na taa slika
     :param train: ako sakas da procitas mnozestvo na sliki togas odredi dali go sakas mnozestvoto za treniranje ili za
     predviduvanje
-    :return vraka python recnik od sliki so nivniot img_id ili dokolku img_id ne e None togas vraka edna slika; Slikite se numpy.array() so
-    dimenzii mxnx3:
+    :return vraka python recnik od sliki so nivniot img_id ili dokolku img_id ne e None togas vraka edna slika; Slikite
+    se numpy.array() so dimenzii mxnx3:
     """
     df = pd.read_csv(os.path.join('train_wkt_v4.csv', 'train_wkt_v4.csv'))
     image_ids = df['ImageId'].unique()
@@ -18,14 +18,14 @@ def read_3band(img_id=None, train=True):
     if not img_id:
         if train:
                 for file in os.listdir('three_band'):
-                    id = file.strip('.tif')
-                    if id in image_ids:
-                        images[id] = (tiff.imread(os.path.join('three_band', file)).transpose([1, 2, 0]))
+                    timg_id = file.strip('.tif')
+                    if timg_id in image_ids:
+                        images[timg_id] = (tiff.imread(os.path.join('three_band', file)).transpose([1, 2, 0]))
         else:
             for file in os.listdir('three_band'):
-                id = file.strip('.tif')
-                if id not in image_ids:
-                    images[id] = (tiff.imread(os.path.join('three_band', file)).transpose([1, 2, 0]))
+                timg_id = file.strip('.tif')
+                if timg_id not in image_ids:
+                    images[timg_id] = (tiff.imread(os.path.join('three_band', file)).transpose([1, 2, 0]))
     else:
         return tiff.imread(os.path.join('three_band', img_id + '.tif')).transpose([1, 2, 0])
 
