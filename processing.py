c0d28640f797035deb7110cd537bafebcb606cd3
Filip-PC <kinez.didi@gmail.com>
2017-01-28 02:47:42 +0100

    funkcija za kreiranje na poligoni vo slika

--- processing.py
+++ processing.py
@@ -1,19 +1,24 @@
 from reading import *
 import numpy as np
+import shapely.affinity
+import cv2
 
 
-def get_scalers(x_max, y_min, im_size=(3349, 3396)):
+IM_SIZE = (3349, 3396)
+
+
+def get_scalers(grids, im_size= IM_SIZE):
     """
-    Se dobivaat skaliracki faktori za koordinatite na poligonite za da mozat da se konvertiraat vo pikseli
-    :param x_max: Se zima od grid_sizes posebno za sekoja slika
-    :param y_min: Se zima od grid_sizes posebno za sekoja slika
-    :param im_size: golemina na slikata so koja se raboti
-    :return:
+
+    :param grids: grids_siizes.csv DataFrame
+    :param im_size: golemina na slikata
+    :return DataFrame so dopolnitelni koloni za skalirackite faktori x_ i y_:
     """
     h, w = im_size  # they are flipped so that mask_for_polygons works correctly
     w_ = w*(w/(w + 1))
     h_ = h*(h/(h + 1))
-    return w_/x_max, h_/y_min
+    grids['x_'], grids['y_'] = w_/grids['Xmax'], h_/grids['Ymin']
+    return grids
 
 
 def imadjust(image, lower_percent=2, higher_percent=98):
@@ -49,3 +54,25 @@ def imadjust(image, lower_percent=2, higher_percent=98):
 
         out[:, :] = t
         return out.astype(np.uint8)
+
+
+def polygon_masks():
+    wkt = read_polygons()
+    grids = read_grid_sizes()
+    grids = grids[grids['ImageId'].isin(wkt['ImageId'])]
+    grids = get_scalers(grids).set_index('ImageId')
+    masks = {'ImageId': [], 'Mask': []}
+
+    for id in grids.index.tolist():
+        x_, y_ = grids.loc[id].x_, grids.loc[id].y_
+        mask = np.zeros(IM_SIZE, dtype=np.uint8)
+        polys = wkt[wkt.ImageId == id]['MultipolygonWKT'].tolist()
+        for mpoly, i in zip(polys, range(len(polys))):
+                pom = [shapely.affinity.scale(poly, xfact=x_, yfact=y_, origin=(0, 0, 0)) for poly in mpoly]
+                exteriors = [np.array(poly.exterior.coords).round().astype(np.int32) for poly in pom]
+                interiors = [np.array(pi.coords).round().astype(np.int32) for poly in pom for pi in poly.interiors]
+                cv2.fillPoly(mask, exteriors, i+1)
+                cv2.fillPoly(mask, interiors, 0)
+        masks['ImageId'].append(id)
+        masks['Mask'].append(mask)
+    return pd.DataFrame.from_dict(masks)
